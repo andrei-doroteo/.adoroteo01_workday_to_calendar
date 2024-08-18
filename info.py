0906bae2dfb7fd2bae2659a4159866fb989ffd59
Adoroteo01 <andreitot13@gmail.com>
2024-08-18 13:10:49 -0600

    add handle_meeting_patterns() and a test for it

--- info.py
+++ info.py
@@ -1,6 +1,10 @@
+### Andrei Doroteo
+### 2024/08/17
+
+
 def get_name(section:str, name:str="") -> str:
     """
-    consumes a section provided by workday. produces the class name in the format "<class name> <class number> <section>"
+    consumes a section provided by workday. produces the class name in the format "<class name> <class number> <section>".
     """
 
     try:
@@ -14,6 +18,32 @@ def get_name(section:str, name:str="") -> str:
             return get_name(section=section[1:], name = name + section[0])
     
     except IndexError:
-        print("Invalid Section") # !!!
+        print("Invalid Section") # !!! Handle error better in final app
         return ""
 
+
+def handle_meeting_patterns(mp:str, days:str="", time:str="", room:str="", counter:int=0) -> dict:
+    """
+    consumes meeting pattern on workday schedule. produces a dict of days, time, and room of the course
+    """
+    # 2024-09-03 - 2024-12-06 | Mon Wed Fri | 2:00 p.m. - 3:00 p.m. | WESB-Floor 1-Room 100 !!!
+    if mp == "":
+       return {"days":convert_days(days), "time":convert_time(time), "room":convert_room(room)}
+    
+    else:
+       if mp[0] == " " and mp[1] == "|" and mp[2] == " ":
+            return handle_meeting_patterns(mp=mp[3:], days=days, time=time, room=room, counter = counter + 1)
+       
+       else:
+            if counter == 0:
+                return handle_meeting_patterns(mp=mp[1:], counter=counter)
+            
+            elif counter == 1:
+                return handle_meeting_patterns(mp=mp[1:], days = days + mp[0], counter=counter)
+            
+            elif counter == 2:
+                return handle_meeting_patterns(mp=mp[1:], days=days, time = time + mp[0], counter=counter)
+            
+            elif counter == 3:
+                return handle_meeting_patterns(mp=mp[1:], days=days, time=time, room = room + mp[0], counter=counter)
+   
--- tests/test_info.py
+++ tests/test_info.py
@@ -16,4 +16,8 @@ def test_get_name_break_begining():
 def test_get_name_no_break():
     assert info.get_name(section="CPSC 200 201") == ""
 
-### 
+### handle_meeting_patterns() tests
+
+def test_handle_meeting_patterns():
+    assert info.handle_meeting_patterns(mp="2024-09-03 - 2024-12-06 | Mon Wed Fri | 2:00 p.m. - 3:00 p.m. | WESB-Floor 1-Room 100") \
+    == {"days":"Mon Wed Fri", "time":"2:00 p.m. - 3:00 p.m.", "room":"WESB-Floor 1-Room 100"}
