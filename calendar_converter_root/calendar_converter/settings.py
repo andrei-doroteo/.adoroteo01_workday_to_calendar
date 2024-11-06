c9b7b6def233896f7ac9d00c367607bffaead871
Adoroteo01 <andreitot13@gmail.com>
2024-11-05 19:35:59 -0800

    configured https

--- calendar_converter_root/calendar_converter/settings.py
+++ calendar_converter_root/calendar_converter/settings.py
@@ -13,8 +13,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj_database_url
 
+
+# Ensure Django only sends cookies over HTTPS
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# Redirect all HTTP traffic to HTTPS
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
