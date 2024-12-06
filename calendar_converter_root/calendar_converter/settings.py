b73cef08299938f251860f6b10d08e5430b9c5ed
Adoroteo01 <andreitot13@gmail.com>
2024-12-06 00:31:11 -0800

    refactor

--- calendar_converter_root/calendar_converter/settings.py
+++ calendar_converter_root/calendar_converter/settings.py
@@ -20,8 +20,8 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
 # Redirect all HTTP traffic to HTTPS
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
