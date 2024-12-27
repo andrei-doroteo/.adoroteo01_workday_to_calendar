2de90e04e7b7882ec1febb8e32c77f38e8258880
Adoroteo01 <andreitot13@gmail.com>
2024-12-26 21:03:06 -0800

    specified tests for main.py

--- calendar_converter_root/converter/utils/tests/test_main.py
+++ calendar_converter_root/converter/utils/tests/test_main.py
@@ -1,13 +1,32 @@
 import sys
-sys.path.append("./")
-from ..main import *
 
-expected = read_excel('data/View_My_Courses_1.xlsx', skiprows=3)
+from pandas import read_excel
+
+from calendar_converter_root.converter.utils.main import import_data
+
+sys.path.append("./")
+
+expected = read_excel("data/View_My_Courses_1.xlsx", skiprows=3)
 
 
 def test_import_data_desktop():
-    assert import_data('data/View_My_Courses_1.xlsx').equals(expected)
+    assert import_data("data/View_My_Courses_1.xlsx").equals(expected)
 
 
 def test_import_data_mobile():
-    assert import_data('data/View_My_Courses.xlsx').equals(expected)
+    assert import_data("data/View_My_Courses.xlsx").equals(expected)
+
+
+def test_find_start():
+    # TODO
+    pass
+
+
+def test_find_end():
+    # TODO
+    pass
+
+
+def test_get_data():
+    # TODO
+    pass
