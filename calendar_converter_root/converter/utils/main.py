eef1e14c288d0145821e9f9be97ba620603f7807
Adoroteo01 <andreitot13@gmail.com>
2024-12-26 17:11:11 -0800

    reformated main.py

--- calendar_converter_root/converter/utils/main.py
+++ calendar_converter_root/converter/utils/main.py
@@ -6,59 +6,65 @@ from pandas import DataFrame
 
 
 def import_data(file: UploadedFile) -> DataFrame:
-    """ """
+    """
+    Inputs:
+    - file: a file upload of a UBC workday class schedule
 
-    def find_start(file: UploadedFile) -> int:
-        # TODO
-        """
-        Inputs:
-        - file
-            - a file upload of a UBC workday class schedule
+    Returns:
+    - a Dataframe of the schdeule content of the uploaded UBC workday
+    schedule
+    """
 
-        Returns:
-        - an int of the index of the begining row of (the column names)
-        the schedule data
+    start = _find_start(file)
+    end = _find_end(file)
 
-        """
-
-        return 0  # stub
-
-    def find_end(file: UploadedFile) -> int:
-        # TODO:
-        """
-        Inputs:
-        - file
-            - a file upload of a UBC workday class schedule
-
-        Returns:
-        - an int of the index of the ending row of the schedule data
-        (the last enrolled course)
-
-        """
-
-        return 0  # stub
-
-    def get_data(file: UploadedFile, start: int, end: int) -> DataFrame:
-        # TODO:
-        """
-        Inputs:
-        - file
-            - a file upload of a UBC workday class schedule
-
-        Returns:
-        - an int of the index of the ending row of the schedule data
-        (the last enrolled course)
-
-        """
-
-        return DataFrame({})  # stub
-
-    start = find_start(file)
-    end = find_end(file)
-
-    data = get_data(file, start, end)
+    data = _get_data(file, start, end)
 
     return data
+
+
+def _find_start(file: UploadedFile) -> int:
+    # TODO
+    """
+    Inputs:
+    - file: a file upload of a UBC workday class schedule
+
+    Returns:
+    - an int of the index of the begining row of (the column names)
+    the schedule data
+
+    """
+
+    return 0  # stub
+
+
+def _find_end(file: UploadedFile) -> int:
+    # TODO:
+    """
+    Inputs:
+    - file: a file upload of a UBC workday class schedule
+
+    Returns:
+    - an int of the index of the ending row of the schedule data
+    (the last enrolled course)
+
+    """
+
+    return 0  # stub
+
+
+def _get_data(file: UploadedFile, start: int, end: int) -> DataFrame:
+    # TODO:
+    """
+    Inputs:
+    - file: a file upload of a UBC workday class schedule
+
+    Returns:
+    - a dataframe of file's
+
+    """
+
+    return DataFrame({})  # stub
 
 
 def convert_file(file: UploadedFile) -> Calendar:
@@ -67,10 +73,4 @@ def convert_file(file: UploadedFile) -> Calendar:
     converted = manipulator.convert_all(data)
     data_dict = converted.to_dict(orient="records")
 
-    # cal_name = input("Save as: ")
     return scheduling.create_ical(data_dict)
-
-
-# path = input("Enter file path: ")
-
-# convert_file(path)
