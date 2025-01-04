d4cfffd1c5d7c02f2495193d49742bc8a440f1e3
Adoroteo01 <andreitot13@gmail.com>
2025-01-03 22:53:42 -0800

    added tests for _find_start function in transform module

--- tests/packages/converter/test_transform.py
+++ tests/packages/converter/test_transform.py
@@ -2,7 +2,7 @@ import sys
 
 from pandas import read_excel
 
-from src.packages.converter.transform import import_data
+from src.packages.converter.transform import import_data, _find_start
 
 sys.path.append("./")
 
@@ -18,8 +18,8 @@ def test_import_data_mobile():
 
 
 def test_find_start():
-    # TODO
-    pass
+    assert _find_start("data/View_My_Courses_1.xlsx") == 2
+    assert _find_start("data/View_My_Courses_2.xlsx") == 5
 
 
 def test_find_end():
