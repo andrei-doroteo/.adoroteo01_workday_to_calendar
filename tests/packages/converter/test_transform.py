a8dd746360d8496a603ce43ac08ce785741783bf
Adoroteo01 <andreitot13@gmail.com>
2025-01-08 08:24:21 -0800

    added tests for transform module in converter package

--- tests/packages/converter/test_transform.py
+++ tests/packages/converter/test_transform.py
@@ -2,20 +2,27 @@ import sys
 
 from pandas import read_excel
 
-from src.packages.converter.transform import (_find_end, _find_start,
-                                              import_data)
+from src.packages.converter.transform import (
+    _find_end,
+    _find_start,
+    import_data,
+)
 
 sys.path.append("./")
 
 expected = read_excel("data/View_My_Courses_1.xlsx", skiprows=2)
+expected2 = read_excel("data/View_My_Courses_2.xlsx", skiprows=5, skipfooter=7)
+expected3 = read_excel("data/View_My_Courses_3.xlsx", skiprows=2)
 
 
 def test_import_data_desktop():
-    assert import_data("data/View_My_Courses_1.xlsx").equals(expected)
+    assert import_data("data/View_My_Courses_2.xlsx").equals(expected2)
+    # !!! add desktop version of View_My_Courses_3.xlsx
 
 
 def test_import_data_mobile():
     assert import_data("data/View_My_Courses_1.xlsx").equals(expected)
+    assert import_data("data/View_My_Courses_3.xlsx").equals(expected3)
 
 
 def test_find_start():
