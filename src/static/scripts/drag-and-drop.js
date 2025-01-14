619fc64b7efe7b39c1d475610f1a69f949fe8e55
Adoroteo01 <andreitot13@gmail.com>
2025-01-13 16:07:35 -0800

    implemented new upload section design

--- src/static/scripts/drag-and-drop.js
+++ src/static/scripts/drag-and-drop.js
@@ -4,51 +4,47 @@ const fileInput = document.getElementById("file-input");
 const uploadButton = document.getElementById("upload-button");
 const browseButton = document.getElementById("browse-files-button");
 
-fileUploadForm.addEventListener('dragover', fileUploadDragOver);
-fileUploadForm.addEventListener('dragenter', fileUploadDragEnter);
-fileUploadForm.addEventListener('dragleave', fileUploadDragLeave);
-fileUploadForm.addEventListener('drop', fileUploadDrop);
-fileInput.addEventListener('change', fileInputChange);
-browseButton.addEventListener('click', browseButtonClick);
-
+fileUploadForm.addEventListener("dragover", fileUploadDragOver);
+fileUploadForm.addEventListener("dragenter", fileUploadDragEnter);
+fileUploadForm.addEventListener("dragleave", fileUploadDragLeave);
+fileUploadForm.addEventListener("drop", fileUploadDrop);
+fileInput.addEventListener("change", fileInputChange);
+browseButton.addEventListener("click", browseButtonClick);
 
 // Event Listeners
 function fileUploadDragOver(event) {
-    event.preventDefault();
+	event.preventDefault();
 }
 
 function fileUploadDragEnter(event) {
-    fileUploadForm.classList.add('over');
-    uploadButton.classList.add('over');
+	fileUploadForm.classList.add("over");
+	browseButton.classList.add("over");
 }
 
 function fileUploadDragLeave(event) {
-    fileUploadForm.classList.remove('over');
-    uploadButton.classList.remove('over');
+	fileUploadForm.classList.remove("over");
+	browseButton.classList.remove("over");
 }
 
 function fileUploadDrop(event) {
-    event.preventDefault();
-    fileUploadForm.classList.remove('over');
-    uploadButton.classList.remove('over');
-    const file = event.dataTransfer.files[0];
+	event.preventDefault();
+	fileUploadForm.classList.remove("over");
+	browseButton.classList.remove("over");
+	const file = event.dataTransfer.files[0];
 
-
-
-    if (file) {
-        fileInput.files = event.dataTransfer.files; // Update input files
-        fileName.querySelector('strong').textContent = file.name; // TODO: make it so the name switches whenever the fileInput file is changed
-    }
+	if (file) {
+		fileInput.files = event.dataTransfer.files; // Update input files
+		fileName.querySelector("strong").textContent = file.name; // TODO: make it so the name switches whenever the fileInput file is changed
+	}
 }
 
 function browseButtonClick(event) {
-    fileInput.click();
+	fileInput.click();
 }
 
 function fileInputChange(event) {
-
-    const file = fileInput.files[0];
-    if (file) {
-        fileName.querySelector('strong').textContent = file.name; // TODO: make it so the name switches whenever the fileInput file is changed
-    }
+	const file = fileInput.files[0];
+	if (file) {
+		fileName.querySelector("strong").textContent = file.name; // TODO: make it so the name switches whenever the fileInput file is changed
+	}
 }
